a3a78df4075eeda259d28d6e1b131f9afa85b2e5
Eduardo Garcia <a23110167@ceti.mx>
2024-07-01 02:57:19 -0600

    Avance semifinal de proyecto

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <stdexcept>
+#include <Box2D/Box2D.h>
 
 class Snake
 {
@@ -19,6 +20,26 @@ public:
         }
         this->sprite = sf::Sprite(texture);
         this->sprite.setPosition(position); // Posición inicial sprite
+     // Mapa
+       
+
+        fondo.setSize(sf::Vector2f(640, 499));
+        fondo.setPosition(position); // Posición inicial cuadro
+        fondo.setFillColor(color = sf::Color::Yellow);
+          
+
+        // Cargar la imagen desde un archivo
+        
+        if (!mapa.loadFromFile("./assets/images/mapa.png"))
+        {
+            throw std::runtime_error("No se cargo la imagen");
+        }
+        this->recorte = sf::Sprite(mapa);
+        this->recorte.setPosition(sf::Vector2f(0.0f, 0.0f)); 
+
+       // Paredes 
+    
+
     }
 
     void moveL(float offsetX, float offsetY)
@@ -52,7 +73,9 @@ public:
     void draw(sf::RenderWindow &window)
     {
         window.draw(this->shape);
+        window.draw(this->recorte);
         window.draw(this->sprite);
+        
     }
 
 
@@ -64,63 +87,52 @@ sprite.setTextureRect(sf::IntRect(1, 1, 50, 40));
 
 private:
     sf::RectangleShape shape;
+    sf::RectangleShape fondo;
     sf::Sprite sprite;
+    sf::Sprite recorte;
     sf::Texture texture;
+    sf::Texture mapa;
     sf::Clock clock;
     float frameTime = 0.1f; // Tiempo entre cada frame en segundos
     int currentFrame = 0;
-    int numFrames = 4; // Número total de frames en la animación
     int frameWidth = 32;
     int frameHeight = 32;
 };
 
-double velocidad = 0.2;
+double velocidad = 0.1;
 
-class Mapa{
-
-public:
-   
-    Mapa(sf::Vector2f position, sf::Color color)
-    {
-        formaMapa.setSize(sf::Vector2f(50, 50));
-        formaMapa.setPosition(position); // Posición inicial cuadro
-        formaMapa.setFillColor(color = sf::Color::Black);
-          
-
-        // Cargar la imagen desde un archivo
-        
-        if (!textura.loadFromFile("./assets/images/mapa.png"))
-        {
-            throw std::runtime_error("No se cargo la imagen");
-        }
-        this->recorte = sf::Sprite(textura);
-        this->recorte.setPosition(position); // Posición inicial sprite
-    }
-    void Cargar(){
-
-    formaMapa.setTextureRect(sf::IntRect(1, 1,640, 499));
-
-    }
-    
-     void draw(sf::RenderWindow &window)
-    {
-        window.draw(this->formaMapa);
-        window.draw(this->recorte);
-    }
-
-    private:
-    sf::RectangleShape formaMapa;
-    sf::Sprite recorte;
-    sf::Texture textura;
-    sf::Clock clock;
-};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "DinoChrome");
+    sf::RenderWindow window(sf::VideoMode(640, 499), "juegoSnake");
 
-    Snake snake(sf::Vector2f(400, 300), sf::Color::Red);
-    Mapa  mapa (sf::Vector2f(400, 300), sf::Color::Yellow);
+    Snake snake(sf::Vector2f(400, 300), sf::Color::Black);
+  
+
+   //Paredes 
+
+    // Pared superior
+    sf::RectangleShape techo(sf::Vector2f(800.0f, 10.0f));
+    techo.setFillColor(sf::Color::Red);
+    techo.setPosition(0.0f, 0.0f);
+
+    // Pared inferior
+    sf::RectangleShape bottomWall(sf::Vector2f(800.0f, 10.0f));
+    bottomWall.setFillColor(sf::Color::Red);
+    bottomWall.setPosition(0.0f, 590.0f);
+
+    // Pared izquierda
+    sf::RectangleShape leftWall(sf::Vector2f(10.0f, 600.0f));
+    leftWall.setFillColor(sf::Color::Red);
+    leftWall.setPosition(0.0f, 0.0f);
+
+    // Pared derecha
+    sf::RectangleShape rightWall(sf::Vector2f(10.0f, 600.0f));
+    rightWall.setFillColor(sf::Color::Red);
+    rightWall.setPosition(790.0f, 0.0f);
+ // Reloj
+
+  // sf::Clock clock;
 
     while (window.isOpen())
     {
@@ -132,6 +144,12 @@ int main()
                 window.close();
             }
         }
+      
+      //Ver tiempo transcurrido 
+       /* sf::Time deltaTime = clock.restart();
+        float dt = deltaTime.asSeconds();*/
+       
+       //Mover serpiente
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
@@ -153,16 +171,31 @@ int main()
             snake.moveD(0, velocidad*1);
         }
 
-        // Actualizar animacion pikachu
-        snake.update();
+        /* Detectar colisiones con las paredes
+        if (snake.getGlobalBounds().intersects(techo.getGlobalBounds())) {
+            snake.setPosition(snake.getPosition().x, techo.getPosition().y + techo.getSize().y);
+        }
+        if (snake.getGlobalBounds().intersects(bottomWall.getGlobalBounds())) {
+            snake.setPosition(player.getPosition().x, bottomWall.getPosition().y - player.getSize().y);
+        }
+        if (player.getGlobalBounds().intersects(leftWall.getGlobalBounds())) {
+            player.setPosition(leftWall.getPosition().x + leftWall.getSize().x, player.getPosition().y);
+        }
+        if (player.getGlobalBounds().intersects(rightWall.getGlobalBounds())) {
+            player.setPosition(rightWall.getPosition().x - player.getSize().x, player.getPosition().y);
+        }*/
+
+        // Actualizar mapa
         window.clear();
-        snake.draw(window);
+        snake.update();
+
+        window.draw(techo);
+        window.draw(bottomWall);
+        window.draw(leftWall);
+        window.draw(rightWall);
+        snake.draw(window);   
+        
         window.display();
-
-        //mapa.Cargar();
-        //mapa.draw(window);
-       // window.display();
-
 
     }
 
